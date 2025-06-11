ac323c2b90f40cbbaaf0b4e7641a88e11879dd88
Ronald Yamil Cuellar Butrón <u202223827@upc.edu.pe>
2025-06-11 05:00:56 -0500

    ListarCategoria

--- EDIA/src/app/app.routes.ts
+++ EDIA/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CategoriaComponent } from './components/categoria/categoria.component';
+import { InsertarcategoriaComponent } from './components/categoria/insertarcategoria/insertarcategoria.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+       path:'categoria',component:CategoriaComponent,
+        children:[
+            {
+                path:'nuevo',component:InsertarcategoriaComponent
+            }
+        ] 
+    }
+];
