b1be9c5f0a29ee9d42dc34cdead7a9872c32d2b4
EBCT <u202220034@upc.edu.pe>
2025-06-12 01:36:30 -0500

    INSERTAR USUARIO

--- EDIA/src/app/app.routes.ts
+++ EDIA/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CategoriaComponent } from './components/categoria/categoria.component';
 import { InsertarcategoriaComponent } from './components/categoria/insertarcategoria/insertarcategoria.component';
+import { UsuarioComponent } from './components/usuario/usuario.component';
+import { InsertarusuarioComponent } from './components/usuario/insertarusuario/insertarusuario.component';
 
 export const routes: Routes = [
     {
@@ -8,6 +10,14 @@ export const routes: Routes = [
         children:[
             {
                 path:'nuevo',component:InsertarcategoriaComponent
+            }
+        ]
+    },
+    {
+        path:'usuario', component: UsuarioComponent,
+        children: [
+            {
+                path: 'nuevo',component: InsertarusuarioComponent
             }
         ] 
     }
--- EDIA/src/app/services/usuario.service.ts
+++ EDIA/src/app/services/usuario.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Usuario } from '../models/usuario';
+import { Subject } from 'rxjs';
+
 const base_url = environment.base
 @Injectable({
   providedIn: 'root'
@@ -10,9 +12,19 @@ export class UsuarioService {
 
   private url = `${base_url}/usuarios`
 
+  private listaUsuarioCambio = new Subject<Usuario[]>();
   constructor(private http: HttpClient) { }
 
   list() {
     return this.http.get<Usuario[]>(this.url)
+  }
+  insert(u: Usuario) {
+    return this.http.post(this.url, u);
+  }
+  getList() {
+    return this.listaUsuarioCambio.asObservable();
+  }
+  setList(listaNueva: Usuario[]) {
+    this.listaUsuarioCambio.next(listaNueva);
   }
 }
