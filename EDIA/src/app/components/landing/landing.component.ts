58eced05fe59a2829aef618667f218fe8f47eb5e
EBCT <u202220034@upc.edu.pe>
2025-07-02 17:16:41 -0500

    BOTON ACTUALIZADO PARA UP

--- EDIA/src/app/components/landing/landing.component.ts
+++ EDIA/src/app/components/landing/landing.component.ts
@@ -46,22 +46,27 @@ export class LandingComponent implements OnInit {
   sendMessage() {
   if (!this.userInput.trim()) return;
 
-  const userMsg = this.userInput;
-  this.messages.push({ from: 'user', text: userMsg });
-  this.userInput = '';
+    const userMsg = this.userInput;
+    this.messages.push({ from: 'user', text: userMsg });
+    this.userInput = '';
 
-  this.hfService.sendToAI(userMsg).subscribe({
-    next: res => {
-    console.log('Respuesta IA:', res);
-    const reply = res?.choices?.[0]?.message?.content || 'No entendí tu pregunta.';
-    const formatted = reply.replace(/\n/g, '<br>').replace(/\*\*(.*?)\*\*/g, '<b>$1</b>');
-    this.messages.push({ from: 'bot', text: formatted });
-  },
-    error: () => {
-      this.messages.push({ from: 'bot', text: 'Hubo un error al procesar tu mensaje.' });
-    }
-  });
-}
+    this.hfService.sendToAI(userMsg).subscribe({
+      next: res => {
+      console.log('Respuesta IA:', res);
+      const reply = res?.choices?.[0]?.message?.content || 'No entendí tu pregunta.';
+      const formatted = reply.replace(/\n/g, '<br>').replace(/\*\*(.*?)\*\*/g, '<b>$1</b>');
+      this.messages.push({ from: 'bot', text: formatted });
+    },
+      error: () => {
+        this.messages.push({ from: 'bot', text: 'Hubo un error al procesar tu mensaje.' });
+      }
+    });
 
+  }
+
+  scrollToTop(): void {
+    const navbar = document.getElementById('navbar');
+    navbar?.scrollIntoView({ behavior: 'smooth' });
+  }
 
 }
