79fb0624a3bb11d14f2e8aad9f15499d5a751a66
EBCT <u202220034@upc.edu.pe>
2025-07-02 01:59:26 -0500

    OPEN AI COMPLETADO

--- EDIA/src/app/services/ia-api.service.ts
+++ EDIA/src/app/services/ia-api.service.ts
@@ -11,17 +11,18 @@ export class IaApiService {
 
   sendToAI(prompt: string): Observable<any> {
   const headers = new HttpHeaders({
-    'Authorization': 'Bearer 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',
-    'Content-Type': 'application/json'
+    'Authorization': 'Bearer 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',
+    'Content-Type': 'application/json',
   });
 
   const body = {
-    inputs: prompt,
-    parameters: { max_new_tokens: 100 }
-  };
+      model: 'deepseek/deepseek-r1:free',
+      messages: [
+        { role: 'system', content: 'Eres un asistente experto en programación y aprendizaje técnico.' },
+        { role: 'user', content: prompt }
+      ]
+    };
 
-  const url = 'https://api-inference.huggingface.co/models/google/flan-t5-small';
-
-  return this.http.post(url, body, { headers });
+  return this.http.post('https://openrouter.ai/api/v1/chat/completions', body, { headers });
 }
 }
